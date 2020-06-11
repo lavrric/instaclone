ada114313020aebb8f699ac23d31094c9bf67495
Mihail <mlavric64@gmail.com>
2020-06-11 22:46:15 +0300

    Updates

---
+++ precache-manifest.e32763e8ae7a32a13f10332467467aad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2585373716bb07c8841933215ebaba",
+    "url": "/instaclone/index.html"
+  },
+  {
+    "revision": "e4353809d28a912d4e5b",
+    "url": "/instaclone/static/css/main.3492a8d1.chunk.css"
+  },
+  {
+    "revision": "7562718ad0cefe9d3745",
+    "url": "/instaclone/static/js/2.1d0cb270.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/instaclone/static/js/2.1d0cb270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4353809d28a912d4e5b",
+    "url": "/instaclone/static/js/main.13ee2948.chunk.js"
+  },
+  {
+    "revision": "34e586b571123be5b99e",
+    "url": "/instaclone/static/js/runtime-main.492e90e8.js"
+  },
+  {
+    "revision": "57d7ccacdbbc8dc94496ae477c2a28ac",
+    "url": "/instaclone/static/media/close.57d7ccac.svg"
+  },
+  {
+    "revision": "41a00f705736de3f0f30e020bbcafcac",
+    "url": "/instaclone/static/media/heart.41a00f70.svg"
+  },
+  {
+    "revision": "523ef8028635352d27c1522d16ce16c2",
+    "url": "/instaclone/static/media/home.523ef802.svg"
+  },
+  {
+    "revision": "04c140e467ac1b7057b6cae1986b1e35",
+    "url": "/instaclone/static/media/next.04c140e4.svg"
+  },
+  {
+    "revision": "b431572c1d6e846808342cfc3eb4bbf5",
+    "url": "/instaclone/static/media/prev.b431572c.svg"
+  }
+]);
